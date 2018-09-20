aac389d9272bf42a0f66a5dc3715d031e8a232d7
anthonykoch <anthonykoch@users.noreply.github.com>
2018-09-19 21:11:14 -0700

    Remove unused file

--- index.js
+++
@@ -1,8 +0,0 @@
-const next = require('next')
-const server = require('./feathers-server')
-
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-
-app.prepare().then(server(handle))
